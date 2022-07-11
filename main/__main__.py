c9345a8a6d8b5278e46efad89c3b35672cd35a3d
Muhammed Shamil <105405467+Muhammedshamil03@users.noreply.github.com>
2022-07-11 19:08:04 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -16,7 +16,7 @@ for name in files:
         load_plugins(plugin_name.replace(".py", ""))
         
 print("Successfully deployed!")
-print("@MaheshChauhan • @DroneBots")
+print("@shamil_shaz_1 • @ML_official_channel")
 
 if __name__ == "__main__":
     Drone.run_until_disconnected()
